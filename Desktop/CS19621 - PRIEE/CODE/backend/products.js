a12e3d63299a1ac5b7e127151eb6200c33524d28
sweatha1110 <sweatharam11@gmail.com>
2024-05-20 00:34:38 +0530

    finalcommit

---
+++ Desktop/CS19621 - PRIEE/CODE/backend/products.js
@@ -0,0 +1,116 @@
+export const products=[
+    {
+        image: "https://cdn.pixabay.com/photo/2021/08/06/16/14/wicker-baskets-6526674_1280.jpg",
+        name: "Jute bags",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "bag",
+        brand: "Craft",
+        price: 600,
+        rating: 5,
+        numRev: 11,
+        stock: 16
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2015/11/07/17/20/hands-1032312_960_720.jpg",
+        name: "woollen sleeve",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "essentials",
+        brand: "Wool",
+        price: 580,
+        rating: 4.5,
+        numRev: 25,
+        stock: 20
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2019/07/31/16/44/embroidery-4375597_960_720.jpg",
+        name: "Embroidery Crafts",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "art",
+        brand: "just.co",
+        price: 350,
+        rating: 3,
+        numRev: 46,
+        stock: 2
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2024/04/08/16/07/diversity-8683857_1280.jpg",
+        name: "Origami crafts",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "art",
+        brand: "origami",
+        price: 270,
+        rating: 4.5,
+        numRev: 17,
+        stock: 9
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2018/03/18/10/20/nizwa-3236202_1280.jpg",
+        name: "Pots",
+        rating: "5",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "essentials",
+        brand: "rajk",
+        price: 300,
+        rating: 2,
+        numRev: 33,
+        stock: 0
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2013/03/23/04/29/carving-96088_960_720.jpg",
+        name: "Wood carvings",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "art",
+        brand: "wood",
+        price: 120,
+        rating: 4,
+        numRev: 20,
+        stock: 50
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2022/02/22/17/38/clown-figurine-7029286_960_720.jpg",
+        name: "Clown sculpture",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "sculpture",
+        brand: "doll",
+        price: 90,
+        rating: 5,
+        numRev: 14,
+        stock: 4
+    },
+
+
+
+    {
+        image: "https://cdn.pixabay.com/photo/2018/03/01/11/54/wicker-3190506_960_720.jpg",
+        name: "Basket",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "essentials",
+        brand: "wood",
+        price: 500,
+        rating: 5,
+        numRev: 180,
+        stock: 80
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2017/04/26/10/38/sewing-2262286_960_720.jpg",
+        name: "Silk",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "art",
+        brand: "rajk",
+        price: 320,
+        rating: 4.0,
+        numRev: 60,
+        stock: 14
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2023/05/23/15/37/ceramic-8013006_1280.jpg",
+        name: "kitchen ware",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        category: "essentials",
+        brand: "wood",
+        price: 80,
+        rating: 5,
+        numRev: 14,
+        stock: 160
+    }
+]
---
+++ Desktop/CS19621 - PRIEE/CODE/frontend/src/data/data.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-dupe-keys */
+export const data=[
+    {
+        id: 1,
+        image: "https://cdn.pixabay.com/photo/2021/08/06/16/14/wicker-baskets-6526674_1280.jpg",
+        name: "Jute bags",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$60",
+        rating: 5,
+        numRev: 11,
+        stock: 16
+    },
+    {
+        id: 2,
+        image: "https://cdn.pixabay.com/photo/2015/11/07/17/20/hands-1032312_960_720.jpg",
+        name: "Woollen sleeve",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$50",
+        rating: 4.5,
+        numRev: 25,
+        stock: 20
+    },
+    {
+        id: 3,
+        image: "https://cdn.pixabay.com/photo/2019/07/31/16/44/embroidery-4375597_960_720.jpg",
+        name: "Embroidery Crafts",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$90",
+        rating: 3,
+        numRev: 46,
+        stock: 2
+    },
+    {
+        id: 4,
+        image: "https://cdn.pixabay.com/photo/2023/05/23/15/37/ceramic-8013006_1280.jpg",
+        name: "kitchen ware",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$270",
+        rating: 4.5,
+        numRev: 17,
+        stock: 9
+    },
+    {
+        id: 5,
+        image: "https://cdn.pixabay.com/photo/2024/04/08/16/07/diversity-8683857_1280.jpg",
+        name: "Origami crafts",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$40",
+        rating: 2,
+        numRev: 33,
+        stock: 0
+    },
+    {
+        id: 6,
+        image: "https://cdn.pixabay.com/photo/2018/03/18/10/20/nizwa-3236202_1280.jpg",
+        name: "Pots",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$120",
+        rating: 4,
+        numRev: 20,
+        stock: 50
+    },
+    {
+        id: 7,
+        image: "https://cdn.pixabay.com/photo/2013/03/23/04/29/carving-96088_960_720.jpg",
+        name: "Wood carvings",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$130",
+        rating: 5,
+        numRev: 14,
+        stock: 4
+    }
+]
